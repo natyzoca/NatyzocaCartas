6b369b4c0a28da452c9daaaefc3b722a2495dc26
natyzoca <00001091765972sp@al.educacao.sp.gov.br>
2024-12-05 08:01:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Palmeiras',
+    'Quando foi alterado o nome do Palmeiras para Sociedade Esportiva Palmeiras?',
+    'Em 1942'
+)
+
+criaCartao(
+    'Palmeiras',
+    'Quantos títulos do Brasileirão o Palmeiras tem?',
+    '12 títulos'
+)
+
+criaCartao(
+    'Palmeiras',
+    'Qual foi o primeiro título do Palmeiras e quando foi?',
+    'Foi o Campeonato Paulista, em 1920'
+)
+
+criaCartao(
+    'Palmeiras',
+    'Em que ano o Palmeiras foi fundado?',
+    '1914!!'
+)
